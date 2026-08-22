9120aa42ea85dbcd166bf1dc97ea4387d979b02c
unknown <thakurnavjot03@gmail.com>
2026-08-22 17:40:07 +0530

    update

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,58 +1,44 @@
 <footer class="sf-footer">
     <div class="container">
-        <div class="row sf-footer__grid">
-            <div class="col-lg-3 col-md-6">
+        <div class="sf-footer__grid">
+            <div class="sf-footer__brand-col">
                 <a href="/" class="sf-footer__brand">SMART FIT<br><span>CABINETS</span></a>
-                <p>Premium custom cabinetry and architectural joinery for Melbourne's exceptional homes and spaces.</p>
-                <div class="footer-logo">
-                    <a href="/"><img src="{!! asset('assets/frontend/images/logo.png') !!}" class="img-fluid logo" alt="Smart Fit Cabinets"></a>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-6">
-                <h4>Quick Links</h4>
-                <ul class="sf-footer-links">
-                    <li><a href="{{ route('projects') }}">Projects</a></li>
-                    <li><a href="{{ route('kitchens') }}">Kitchens</a></li>
-                    <li><a href="{{ route('joinery') }}">Custom Joinery</a></li>
-                    <li><a href="{{ route('builders-architects') }}">Builders &amp; Architects</a></li>
-                    <li><a href="{{ route('about-us') }}">About</a></li>
-                    <li><a href="{{ route('our-process') }}">Our Process</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-2 col-md-6">
-                <h4>Services</h4>
-                <ul class="sf-footer-links">
-                    <li><a href="{{ route('kitchens') }}">Kitchens</a></li>
-                    <li><a href="{{ route('joinery') }}">Wardrobes</a></li>
-                    <li><a href="{{ route('joinery') }}">Vanities</a></li>
-                    <li><a href="{{ route('joinery') }}">Laundries</a></li>
-                    <li><a href="{{ route('joinery') }}">Entertainment Units</a></li>
-                    <li><a href="{{ route('joinery') }}">Architectural Joinery</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-2 col-md-6">
-                <h4>Information</h4>
-                <ul class="sf-footer-links">
-                    <li><a href="{{ route('contact') }}">Contact</a></li>
-                    <li><a href="{{ route('contact') }}">Request a Quote</a></li>
-                </ul>
-                @if(!empty($content))
-                <h4 class="sf-footer__social-title">Follow Us</h4>
+                <p class="sf-footer__tagline">Premium custom cabinetry and architectural joinery for Melbourne homes and spaces.</p>
                 <ul class="social">
+                    @if(!empty($content))
                     <li><a href="{{ $content->instagram }}" target="_blank" rel="noopener" aria-label="Instagram"><i class="fa fa-instagram"></i></a></li>
                     <li><a href="{{ $content->facebook }}" target="_blank" rel="noopener" aria-label="Facebook"><i class="fa fa-facebook"></i></a></li>
                     <li><a href="{{ $content->linkedin }}" target="_blank" rel="noopener" aria-label="LinkedIn"><i class="fa fa-linkedin"></i></a></li>
+                    @endif
                 </ul>
-                @endif
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div>
+                <h4>Quick Links</h4>
+                <ul class="sf-footer-links">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="{{ route('projects') }}">Projects</a></li>
+                    <li><a href="{{ route('services') }}">Services</a></li>
+                    <li><a href="{{ route('our-process') }}">Our Process</a></li>
+                    <li><a href="{{ route('about-us') }}">About</a></li>
+                    <li><a href="{{ route('contact') }}">Contact</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>Services</h4>
+                <ul class="sf-footer-links">
+                    <li><a href="{{ route('kitchens') }}">Kitchens</a></li>
+                    <li><a href="{{ route('joinery') }}">Custom Joinery</a></li>
+                    <li><a href="{{ route('joinery') }}">Wardrobes</a></li>
+                    <li><a href="{{ route('joinery') }}">Vanities</a></li>
+                    <li><a href="{{ route('builders-architects') }}">Builders &amp; Architects</a></li>
+                </ul>
+            </div>
+            <div class="sf-footer__contact">
                 <h4>Contact</h4>
-                <div class="ovr-txt sf-footer__contact">
-                    <p><strong>Address</strong><br>Unit 5/483B Dohertys Road, Truganina, VIC 3029, Australia</p>
-                    <p><strong>Phone</strong><br><a href="tel:0434991936">0434 991 936</a></p>
-                    <p><strong>Email</strong><br><a href="mailto:info@smartfitcabinets.com">info@smartfitcabinets.com</a></p>
-                    <p class="sf-footer__area">Greater Melbourne Region</p>
-                </div>
+                <p>Unit 5/483B Dohertys Road<br>Truganina VIC 3029</p>
+                <p><a href="tel:0434991936">0434 991 936</a></p>
+                <p><a href="mailto:info@smartfitcabinets.com">info@smartfitcabinets.com</a></p>
+                <a href="{{ route('contact') }}" class="sf-footer__cta">Request a Quote</a>
             </div>
         </div>
         <div class="sf-copyright">
